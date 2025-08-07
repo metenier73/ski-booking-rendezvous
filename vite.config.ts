b021b90dad8d00f0eabf5a8872a0abdf0e291127
metenier <meteniermyriam@yahoo.fr>
2025-08-07 20:21:47 +0200

    maj

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/ski-booking-rendezvous/',
   plugins: [react()],
   resolve: {
     alias: {
